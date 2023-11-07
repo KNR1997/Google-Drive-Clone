6fde04295749bd8fb2fc46624b032135b6b6eafe
KNR1997 <kethakaranasinghe@gmail.com>
2023-11-07 22:25:29 +0530

    UI changes

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TextInput from "@/Components/TextInput.vue";
+import {useForm} from "@inertiajs/vue3";
+
+const form = useForm({
+    search: ''
+})
+</script>
+
+<template>
+    <div class="w-[600px] h-[80px] flex items-center">
+        <TextInput model-value="" type="text"
+                   class="block w-full mr-2"
+                   v-model="search"
+                   autocomplete
+                   placeholder="Search for files and folders"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
